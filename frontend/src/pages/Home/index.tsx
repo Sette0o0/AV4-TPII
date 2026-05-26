c8c97a9fb5681a9794907c2f9b8c927ceaf789d2
Rafael Sette de Araujo <138353834+Sette0o0@users.noreply.github.com>
2026-05-26 16:20:37 -0300

    Fetch and display clients in Home component

--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -1,10 +1,37 @@
+import { Card, CardContent } from "@/components/ui/card";
+import { listarClientes } from "@/services/Clientes";
+import type { Cliente } from "@/types/entidades";
+import { useEffect, useState } from "react";
 
-function Home(){
-  return(
+function Home() {
+  const [clientes, setClientes] = useState<Cliente[]>([]);
+
+  async function carregarClientes() {
+    setClientes(await listarClientes());
+  }
+
+  useEffect(() => {
+    carregarClientes();
+  }, []);
+
+  return (
     <>
-      teste
+      {/* page */}
+      <div>
+        {/* coisinhas */}
+        <div>
+          <Card className="min-w-fit max-w-[min(50%,200px)] w-full">
+            <CardContent className="text-lg flex flex-col gap-4 text-center">
+              <h2>Clientes</h2>
+              <p className="bg-accent rounded py-1 text-muted-foreground">
+                {clientes.length}
+              </p>
+            </CardContent>
+          </Card>
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default Home
+export default Home;
